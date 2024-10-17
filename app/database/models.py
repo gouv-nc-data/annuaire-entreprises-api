3879db6881d655d485768f22ab97e85dc8eec1af
qdanneville <quentin@esperluette.io>
2024-10-17 12:01:01 +1100

    chore: temporaire - modele sqlAlchemy changé pour aller avec la base de tests entreprise

--- app/database/models.py
+++ app/database/models.py
@@ -1,28 +1,16 @@
-from typing import List
-
 from app.database.connection import Base
 
-from sqlalchemy import Column, String, Date, Integer
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer
 
 
-class Etablissement(Base):
-    __tablename__ = "etablissement"
+class Entreprise(Base):
+    __tablename__ = "entreprise"
 
     id = Column(Integer, primary_key=True)
-    nom = Column(String)
+    rid = Column(String, unique=True)
+    sigle = Column(String)
+    enseigne = Column(String)
+    forme_juridique = Column(String)
     adresse = Column(String)
-    date_creation = Column(Date)
-    unite_legale_id: Mapped[String] = mapped_column(ForeignKey("unite_legal.ridet"))
-
-
-class UniteLegale(Base):
-    __tablename__ = "unite_legal"
-
-    ridet = Column(String, primary_key=True)
-    nom_complet = Column(String)
-    date_creation = Column(Date)
-    etablissements: Mapped[List["Etablissement"]] = relationship()
+    code_postal = Column(String)
+    ville = Column(String)
