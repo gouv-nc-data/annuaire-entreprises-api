a6bc56da916e581e7555f6f8cbbb950d417fd9bf
qdanneville <quentin@esperluette.io>
2024-10-24 11:02:19 +1100

    fix: print de développement supprimé

--- app/services/search/search_builder.py
+++ app/services/search/search_builder.py
@@ -5,11 +5,8 @@ from app.services.search.queries.search_by_text import search_by_text
 
 def build_search(search_build):
     query_terms = search_build.search_params.terms
-    print("build search query_terms", search_build.search_params)
 
     if is_ridet(query_terms):
-        print("build search is ridet : ")
         search_build.search_client = search_by_ridet(query_terms)
     else:
-        print("build search is text : ")
         search_build.search_client = search_by_text(search_build.search_params)
