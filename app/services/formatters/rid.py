a625f6b543b3eeb9fa006c51d16c385a5ef47b8c
qdanneville <quentin@esperluette.io>
2024-11-28 16:43:26 +1100

    chore: recherche de rid -> 7 digit et ridet -> 10 digit ajoutée

---
+++ app/services/formatters/rid.py
@@ -0,0 +1,8 @@
+def format_ridet(rid: str, et: str):
+    if not rid:
+        return
+
+    if not et:
+        return
+
+    return rid + et
--- app/services/search/search_builder.py
+++ app/services/search/search_builder.py
@@ -1,4 +1,5 @@
 from app.services.search.parsers.rid import is_rid
+from app.services.search.parsers.ridet import is_ridet
 from app.services.search.queries.search_by_rid import search_by_rid
 from app.services.search.queries.search_by_text import search_by_text
 
@@ -8,6 +9,10 @@ def build_search(search_build):
 
     if is_rid(query_terms):
         search_build.search_client = search_by_rid(query_terms)
+    elif is_ridet(query_terms):
+        # We're doing a rid search for a ridet like term
+        # We will return the enterprise associated with the `établissement` rid | et
+        search_build.search_client = search_by_rid(query_terms[0:7])
     elif query_terms is not None:
         search_build.search_client = search_by_text(search_build.search_params)
     else:
