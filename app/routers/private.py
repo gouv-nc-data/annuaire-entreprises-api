908ca8b05345ff6e55cc4cb8e86ada7bb118515b
qdanneville <quentin@esperluette.io>
2025-01-08 11:03:38 +1100

    chore(typesense): ajout d'une route pour récupérer les collections

--- app/routers/private.py
+++ app/routers/private.py
@@ -5,6 +5,8 @@ from app.typesense.create_collections_and_documents import (
     create_collections_and_documents,
 )
 
+from app.typesense.get_collections import get_collections
+
 router = APIRouter()
 
 
@@ -14,3 +16,11 @@ async def typesense_init(request: Request):
     create_collections_and_documents()
     logger.info("fin indexation")
     return {"status": "ok"}
+
+
+@router.get("/typesense/collections", status_code=200)
+async def typesense_retrieve_collections(request: Request):
+    logger.debug("debut collectionss")
+    collections = get_collections()
+    logger.info("fin collections")
+    return collections
---
+++ app/typesense/get_collections.py
@@ -0,0 +1,16 @@
+from app.typesense.connection import typesense_client
+from loguru import logger
+
+
+def get_collections():
+    try:
+        collections = typesense_client.collections.retrieve()
+    except Exception as e:
+        logger.exception(
+            "An error occurred during retrieving collections for typesense:"
+        )
+        raise e
+
+    print("Typesense collections : ", collections)
+
+    return collections
