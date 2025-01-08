d2bcb02b3bec6082ec222eba12cef54d2684d169
Thomas Guyader <thomas.guyader@gouv.nc>
2025-01-09 08:37:28 +1100

    chore: ajout endpoint health

--- app/routers/public.py
+++ app/routers/public.py
@@ -8,3 +8,8 @@ router = APIRouter()
 @router.get("/recherche")
 async def search_endpoint(request: Request):
     return build_api_response(request)
+
+
+@router.get("/health")
+def health_check():
+    return {"status": "healthy"}
