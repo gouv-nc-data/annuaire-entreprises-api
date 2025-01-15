27c20b9b28a527cb81e288ac14c83e9a1a8b78c5
qdanneville <quentin@esperluette.io>
2025-01-15 12:30:25 +1100

    chore: formattage des données dirigeants modifié

--- app/services/formatters/dirigeants.py
+++ app/services/formatters/dirigeants.py
@@ -9,14 +9,24 @@ def format_dirigeants(dirigeants=None):
     if dirigeants:
         for dirigeant_data in dirigeants:
 
-            dirigeant = dirigeant_data.__dict__
+            dirigeant = dirigeant_data
+
+            # We only send active dirigeant
+            if dirigeant["actif"] == False:
+                break
 
             dirigeant = Dirigeant(
                 nom=get_value(dirigeant, "nom"),
-                role=get_value(dirigeant, "role"),
-                prenoms=get_value(dirigeant, "prenoms"),
-                date_de_naissance=get_value(dirigeant, "date_de_naissance"),
+                date_naissance=get_value(dirigeant, "date_naissance"),
                 nationalite=get_value(dirigeant, "nationalite"),
+                adresse=get_value(dirigeant, "adresse"),
+                code_postal=get_value(dirigeant, "code_postal"),
+                ville=get_value(dirigeant, "ville"),
+                type_personne=get_value(dirigeant, "type_personne"),
+                ordreaffichage=get_value(dirigeant, "ordreaffichage"),
+                numerochrono=get_value(dirigeant, "numerochrono"),
+                actif=get_value(dirigeant, "actif"),
+                fonction=get_value(dirigeant, "fonction"),
             )
 
             dirigeants_formatted.append(dirigeant)
