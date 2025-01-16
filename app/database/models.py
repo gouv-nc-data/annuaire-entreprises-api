09ddfe7621f766327249e1387c75f5087736198e
qdanneville <quentin@esperluette.io>
2025-01-16 14:06:36 +1100

    fix: situation_entreprise ré-ajouté

--- app/database/models.py
+++ app/database/models.py
@@ -39,6 +39,7 @@ class Entreprise(Base):
     date_radiation = Column(Date)
     motif_radiation = Column(String)
     convention_collective = Column(String)
+    situation_entreprise = Column(String)
 
     # Immatriculation RCS
     numero_rcs = Column(String)
