5617575537ffc4fcae4cad5fc5da291d6219413b
Thomas Guyader <thomas.guyader@gouv.nc>
2025-07-17 15:50:20 +1100

    chore(entreprise): ajout attribut type structure

--- app/database/models.py
+++ app/database/models.py
@@ -40,6 +40,7 @@ class Entreprise(Base):
     motif_radiation = Column(String)
     convention_collective = Column(String)
     situation_entreprise = Column(String)
+    type_sctucture = Column(String)
 
     # Immatriculation RCS
     numero_rcs = Column(String)
